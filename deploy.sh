30cf12037b8a8f48bcb7524dc9119404d7a5b82e
Andris Strals <andris.strals@gmail.com>
2018-11-21 10:31:45 +0000

    travis and deployment scripts updated to use git SHA as docker image tag

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
-docker build -t astrals63/multi-client -f ./client/Dockerfile ./client
-docker build -t astrals63/multi-server -f ./server/Dockerfile ./server
-docker build -t astrals63/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t astrals63/multi-client:latest -t astrals63/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t astrals63/multi-server:latest -t astrals63/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t astrals63/multi-worker:latest -t astrals63/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
-docker push astrals63/multi-client
-docker push astrals63/multi-server
-docker push astrals63/multi-worker
+docker push astrals63/multi-client:latest
+docker push astrals63/multi-server:latest
+docker push astrals63/multi-worker:latest
+
+docker push astrals63/multi-client:$GIT_SHA
+docker push astrals63/multi-server:$GIT_SHA
+docker push astrals63/multi-worker:$GIT_SHA
 
 kubectl -f k8s
 
-kubectl set image deployments/server-deployment server=astrals63/multi-server
+kubectl set image deployments/client-deployment client=astrals63/multi-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=astrals63/multi-server:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=astrals63/multi-worker:$GIT_SHA
